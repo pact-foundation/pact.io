3ca7be620684cb3ff05b22b4d8d681bd71642575
Michel Boudreau <michelboudreau@gmail.com>
2017-11-22 14:44:43 +1100

    trying out vue.js with nuxt

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="pa4 code">
+    <div class="bg-white-90 pa4">
+      <div class="f1">Nuxt TypeScript Starter</div>
+      <div class="f3">Selected Person: {{selectedPerson.first_name}} {{selectedPerson.last_name}} is #{{selected}}</div>
+      
+    </div>
+    <div class="flex flex-wrap ph2 justify-between bg-white-80">
+      <div :key="person.id" v-for="person in people">
+        <Card :person="person"></Card>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "nuxt-class-component"
+import Card from "../components/Card.vue"
+import { State, Getter } from "vuex-class"
+
+@Component({
+  components: {
+    Card
+  }
+})
+export default class extends Vue {
+  @State selected;
+  @State people;
+  @Getter selectedPerson;
+}
+</script>
---
+++ store/index.ts
@@ -0,0 +1,31 @@
+import {getPeople} from "../api/people"
+
+export const state = () => ({
+	selected: 1,
+	people: []
+});
+
+export const mutations = {
+	select(state, id) {
+		state.selected = id
+	},
+	setPeople(state, people) {
+		state.people = people
+	}
+};
+
+export const getters = {
+	selectedPerson: state => {
+		const p = state.people.find(person => person.id === state.selected);
+		return p ? p : {first_name: 'Please,', last_name: 'select someone'}
+	}
+};
+
+export const actions = {
+	async nuxtServerInit({commit}) {
+		commit('setPeople', await getPeople(3))
+	},
+	select({commit}, id) {
+		commit('select', id)
+	}
+};
